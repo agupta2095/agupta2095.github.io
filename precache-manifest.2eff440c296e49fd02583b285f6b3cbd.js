daeed3c4aa73c1d58047d0b134d9332298968476
agupta2095 <guptaakanksha2196@yahoo.in>
2024-02-05 15:42:03 -0800

    Updates

---
+++ precache-manifest.2eff440c296e49fd02583b285f6b3cbd.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "132a40b8798200e2bc0015ebe8e427a7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c75f0699b658664d1c4e",
+    "url": "/static/css/main.6a056af1.chunk.css"
+  },
+  {
+    "revision": "bc853196e31e2d32a6c9",
+    "url": "/static/js/2.a2b181a3.chunk.js"
+  },
+  {
+    "revision": "ea935f47a7b92aa14dc7",
+    "url": "/static/js/3.a29efab2.chunk.js"
+  },
+  {
+    "revision": "c75f0699b658664d1c4e",
+    "url": "/static/js/main.f5652016.chunk.js"
+  },
+  {
+    "revision": "cd2988c65fcca3ce3217",
+    "url": "/static/js/runtime~main.b45b9353.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/static/media/logo.06e73328.svg"
+  }
+]);
