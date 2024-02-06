b063a9d9f4cbcc446d8712dc29db27ae41dabbcc
agupta2095 <agupta2095@users.noreply.github.com>
2024-02-06 00:14:22 +0000

    deploy: 1385ab5dab8124997b97b95af2bbd920dec4d88a

---
+++ precache-manifest.e30bfbaba8d2a98ac0d82424478a76a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343d0afb80f689b562570a0ae3adec44",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28fd58ddae0dfb410118",
+    "url": "/static/css/main.6a056af1.chunk.css"
+  },
+  {
+    "revision": "3db01d96c8cd1f9dcdb8",
+    "url": "/static/js/2.51eda38e.chunk.js"
+  },
+  {
+    "revision": "60f6bf9e100e456690e9ab6c9a37bfc2",
+    "url": "/static/js/2.51eda38e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8a075391d889695d89c",
+    "url": "/static/js/3.6a758fe9.chunk.js"
+  },
+  {
+    "revision": "28fd58ddae0dfb410118",
+    "url": "/static/js/main.040ff12d.chunk.js"
+  },
+  {
+    "revision": "9607c944aa214818575e",
+    "url": "/static/js/runtime-main.69103989.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1d46881467055e9048c367f806a88e55.js"
+  "/precache-manifest.e30bfbaba8d2a98ac0d82424478a76a2.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
